aa19b7a8b2f771243243630e80b66436df1a9c1e
PhoenixAve <15565999182@163.com>
2017-04-19 22:42:52 +0800

    'firstCommit'

---
+++ src/components/Hello.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <input type="text">
+    <datePicker></datePicker>
+  </div>
+</template>
+
+<script>
+import datePicker from '@/components/datePicker'
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    datePicker
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+  width: 300px;
+  height: 24px;
+}
+</style>
---
+++ src/components/datePicker.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="datePicker">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="item in weeks">
+            {{item}}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in 5">
+          <td v-for="day in 7">
+            <a href="javascript:;">{{day*item}}</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'datePicker',
+  data () {
+    return {
+      weeks: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+      date: null,
+      time: null
+    }
+  }
+}
+</script>
+<style scoped>
+table {
+  border-collapse: collapse;
+}
+tr, td {
+  width: 50px;
+  height: 50px;
+}
+tr {
+  background-color: #333;
+  color: #fff;
+}
+th, td {
+  border: 1px solid #fff;
+}
+td {
+  background-color: orange;
+}
+a{
+  text-decoration: none;
+  color: #fff;
+}
+</style>
